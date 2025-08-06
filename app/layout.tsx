55ce758cea7df64e87fda40ef3b1fabdeeeb1672
sachinggsingh <sachinggsingh@gmail.com>
2025-08-06 18:27:57 +0530

    will be adding docker

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,16 +2,24 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import ClientWrapper from "./client-wrapper";
-import { Roboto_Mono } from 'next/font/google';
+import { Roboto_Mono } from "next/font/google";
+// import Head from "next/head";
 
 const geistMono = Roboto_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
-})
+});
 
 export const metadata: Metadata = {
   title: "Resume Refiner AI",
   description: "Resume Refiner AI - AI Powered Resume Analyzer",
+  icons: {
+    icon: [
+      { url: "/favicon.ico", sizes: "any" },
+      { url: "/favicon.ico", sizes: "any", type: "image/x-icon" },
+      { url: "/favicon.svg", type: "image/svg+xml" },
+    ],
+  },
 };
 
 export default function RootLayout({
@@ -21,6 +29,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      {/* <Head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </Head> */}
       <body
         className={`${geistMono.variable} ${geistMono.variable} antialiased bg-white text-black dark:bg-black dark:text-white`}
       >
@@ -34,5 +45,5 @@ export default function RootLayout({
         </ThemeProvider>
       </body>
     </html>
-  )
+  );
 }
